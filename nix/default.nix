4201d29fd75707838ae1034ee68dec92c294d9bc
bitterlox <bitterlox@pm.me>
2023-11-16 16:22:31 +0100

    reorganized code;

--- nix/default.nix
+++ nix/default.nix
@@ -1,4 +1,29 @@
 # this is a flake-parts module
 { ... }: {
-  imports = [ ./packages ./options.nix ./nvim ];
+  imports = [ ./packages ./nvim ];
+  perSystem = { pkgs, ... }: {
+    config = {
+      neovim.includeInPath = builtins.attrValues {
+        # lsps
+        inherit (pkgs)
+          gopls lua-language-server rust-analyzer efm-langserver nil shellharden
+          yamllint stylua nixfmt;
+        inherit (pkgs.nodePackages)
+          bash-language-server typescript-language-server jsonlint
+          markdownlint-cli;
+        # tools
+        inherit (pkgs) ripgrep fd;
+      };
+      # option definition expects plugins in this shape
+      # { name::String, package::Derivation }
+      neovim.plugins = import ./plugins.nix pkgs;
+      neovim.pathsToLua = {
+        editorConfig = ../lua/config/editor-config;
+        globals = ../lua/globals;
+        pluginConfig = ../lua/config/plugins/plugin-config;
+        pluginKeyBindings = ../lua/config/plugins/plugin-keybindings;
+        pluginExtraConfig = ../lua/config/plugins/extra-config;
+      };
+    };
+  };
 }
--- nix/nvim/default.nix
+++ nix/nvim/default.nix
@@ -1,34 +1,13 @@
 { lib, ... }: {
+  imports = [ ./options.nix ];
   perSystem = { inputs', config, system, pkgs, ... }:
     let
-      customized-nvim = import ../custom-nvim.nix {
+      customized-nvim = import ./custom-nvim.nix {
         inherit pkgs;
         inherit (config.neovim) includeInPath plugins pathsToLua;
       };
-      # option definition expects plugins in this shape
-      # { name::String, package::Derivation }
-      plugins = import ./plugins.nix pkgs;
     in {
       config = {
-        neovim.includeInPath = builtins.attrValues {
-          # lsps
-          inherit (pkgs)
-            gopls lua-language-server rust-analyzer efm-langserver nil
-            shellharden yamllint stylua nixfmt;
-          inherit (pkgs.nodePackages)
-            bash-language-server typescript-language-server jsonlint
-            markdownlint-cli;
-          # tools
-          inherit (pkgs) ripgrep fd;
-        };
-        neovim.plugins = plugins;
-        neovim.pathsToLua = {
-          editorConfig = ../../lua/config/editor-config;
-          globals = ../../lua/globals;
-          pluginConfig = ../../lua/config/plugins/plugin-config;
-          pluginKeyBindings = ../../lua/config/plugins/plugin-keybindings;
-          pluginExtraConfig = ../../lua/config/plugins/extra-config;
-        };
         packages.default = customized-nvim;
         apps.default = {
           type = "app";
