9b4a7c9143e137e957f4af14d9c679b45d44f007
bitterlox <bitterlox@pm.me>
2024-02-06 13:00:37 +0100

    move themes lua file to avoid double loading it;

--- nix/configurations/addons/plugins/essentials/themes.nix
+++ nix/configurations/addons/plugins/essentials/themes.nix
@@ -3,5 +3,5 @@ let
   customThemes = builtins.attrValues pkgs.vimPlugins.customThemes;
 in addon.makePluginAddon {
   pkg = [ pkgs.vimPlugins.rose-pine ] ++ customThemes;
-  config = [ ../../../../../lua/config/editor-config/themes.lua ];
+  config = [ ../../../../../lua/config/plugins/extra-config/themes.lua ];
 }
